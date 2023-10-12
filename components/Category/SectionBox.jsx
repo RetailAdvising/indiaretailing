79f8d2bceb78cad5acad01a7385b016f4f4798cd
Tridots <info@tridotstech.com>
2023-10-12 20:38:57 +0530

    changes

--- components/Category/SectionBox.jsx
+++ components/Category/SectionBox.jsx
@@ -15,8 +15,8 @@ export default function SectionBox({ data }) {
     return (
         <>
             {data && <div className={`lg:border-[0px] lg:rounded-[10px] md:rounded-[5px_5px_0_0] justify-center p-[10px] md:h-auto lg:h-[280px] flex flex-col md:gap-[5px] lg:gap-[15px]`}  style={{ backgroundColor:`${data.background_color ? data.background_color : '#EDF6E5' }`}}>
-                <p className='text-red lg:text-[14px] md:text-[12px] cursor-pointer'>{data.primary_text}</p>
-                <h6 className='lg:text-[20px] md:text-[15px] line-clamp-[2] font-semibold cursor-pointer'>{data.title}</h6>
+                {/* <p className='text-red lg:text-[14px] md:text-[12px] cursor-pointer'>{data.primary_text}</p> */}
+                <h6 className='lg:text-[20px] md:text-[15px] line-clamp-[2] font-semibold cursor-pointer' onClick={() => router.push(`/categories/${data.route}`)}>{data.title}</h6>
                 <p className={`sub_title line-clamp-[4] cursor-pointer `}>{data.description}</p>
                 <p className='flex gap-[5px] items-center cursor-pointer  seeMore' onClick={()=> router.push(`/categories/${data.route}`)}><span className='text-gray font-medium	 md:text-[12px]'>View All</span><Image className='img md:h-[14px] md:w-[14px]' src={'/categories/arrowright.svg'} alt='arrow' height={16} width={16} /></p>
             </div>}
