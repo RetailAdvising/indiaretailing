839b30a39fd1e14b967784f4aba0e00a9cbc341f
Tridots <info@tridotstech.com>
2023-10-18 16:29:35 +0530

    seo Changes

--- components/common/SEO.jsx
+++ components/common/SEO.jsx
@@ -24,26 +24,26 @@ export default function SEO({ title = "India Reatiling",
                     <title key="title">{`${title} – ${siteName}`}</title>
                     <meta name="description" content={description} />
                     <meta name="theme-color" content="#e21b22" />
-                    <meta key="og_type" property="og:type" content={ogType} />
-                    <meta key="og_title" property="og:title" content={title} />
-                    <meta key="og_description" property="og:description" content={description} />
-                    <meta key="og_locale" property="og:locale" content="en_IE" />
-                    <meta key="og_site_name" property="og:site_name" content={siteName} />
-                    <meta key="og_url" property="og:url" content={canonical ?? domain} />
-                    <meta key="og_site_name" property="og:site_name" content={siteName} />
+                    <meta name="og_type" property="og:type" content={ogType} />
+                    <meta name="og_title" property="og:title" content={title} />
+                    <meta name="og_description" property="og:description" content={description} />
+                    <meta name="og_locale" property="og:locale" content="en_IE" />
+                    <meta name="og_site_name" property="og:site_name" content={siteName} />
+                    <meta name="og_url" property="og:url" content={canonical ?? domain} />
+                    <meta name="og_site_name" property="og:site_name" content={siteName} />
                     <meta
-                        key="og_image"
+                        name="og_image"
                         property="og:image"
                         content={ogImage ?? DEFAULT_OG_IMAGE}
                     />
                     <meta
-                        key="og_image:alt"
+                        name="og_image:alt"
                         property="og:image:alt"
                         content={`${title} | ${siteName}`}
                     />
-                    <meta key="og_image:width" property="og:image:width" content="200" />
-                    <meta key="og_image:height" property="og:image:height" content="200" />
-                    <meta name="og:image:type" property="og:image:type" content="image/jpg" />
+                    <meta name="og_image:width" property="og:image:width" content="1200" />
+                    <meta name="og_image:height" property="og:image:height" content="630" />
+
                     <meta name="robots" content="index,follow" />
 
                     <meta
