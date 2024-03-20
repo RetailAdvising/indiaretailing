704045e554ee9131b0cec34e650abf58c9878762
Tridots <info@tridotstech.com>
2024-03-20 17:17:36 +0530

    updates

--- pages/api/auth/[...auth].js
+++ pages/api/auth/[...auth].js
@@ -3,13 +3,16 @@ import nextAuth from 'next-auth';
 import FacebookProvider from 'next-auth/providers/facebook';
 
 export const authOptions = ({
-    pages:{
+    pages: {
         signIn: '/auth/signin',
     },
     providers: [
         FacebookProvider({
             clientId: process.env.FACEBOOK_CLIENT_ID,
             clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+            // authorizationUrl: 'https://www.facebook.com/v13.0/dialog/oauth',
+            // scope: 'email',
+            // redirectUri: process.env.NEXTAUTH_URL + '/api/auth/callback/facebook',
             // authorization: { params: { scope: 'email',redirect_uri: process.env.NEXTAUTH_URL + "/api/auth/callback/facebook" }},
             // clientId: "341622788230249",
             // clientSecret: "53a83436a46c089d62799997af80a031",
