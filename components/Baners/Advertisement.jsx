62f6cf8b50ac5e7bd227d828dc0a8d5bef07b167
Tridots <info@tridotstech.com>
2024-09-04 18:40:17 +0530

    04-09-24

--- components/Baners/Advertisement.jsx
+++ components/Baners/Advertisement.jsx
@@ -26,7 +26,7 @@ export default function Advertisement({ data, imgClass, divClass,insStyle }) {
                 data && Object.keys(data).length != 0 &&
                 <div onClick={() => window.open(data.ad_link, '_blank')} className={`${divClass ? divClass : ''} ${data.position == 'Header' || data.position == 'Footer' ? 'h-[90px] w-[728px]' : ''}`}>
                     <ImageLoader style={`${imgClass ? imgClass : ''} h-full w-full`} src={isMobile ? data.mobile_image : data.web_image} title={data.title ? data.title : 's'} />
-                </div >
+                </div>
             }
 
             {/* data-ad-format="auto"
