d49c841174c2e8b343b9fc391463bab201485ec7
Tridots <info@tridotstech.com>
2024-09-04 20:23:26 +0530

    04-09-24

--- pages/_app.js
+++ pages/_app.js
@@ -260,6 +260,13 @@ export default function App({ Component, pageProps: { session, ...pageProps } })
           async
           src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js`}
         /> */}
+        <Script
+                // id="adsense-script"
+                // data-ad-client={process.env.NEXT_PUBLIC_GOOGLE_ADS_CLIENT_ID}
+                strategy="beforeInteractive"
+                async
+                src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js`}
+            />
       </Head>
       <ErrorBoundary >
         <Provider store={store} >
