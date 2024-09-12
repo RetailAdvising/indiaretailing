610c5bbd6abade8289785549be6f381e41d0209e
Tridots <info@tridotstech.com>
2024-09-12 12:36:39 +0530

    12-09-24:

--- pages/add-address.js
+++ pages/add-address.js
@@ -1,41 +1,31 @@
 import React, { useState, useEffect } from 'react'
 import MobileHeader from '@/components/Headers/MobileHeader'
-import styles from'../styles/checkout.module.scss';
+import styles from '../styles/checkout.module.scss';
 import { get_customer_info } from '@/libs/api';
 import AddressModal from '@/components/Bookstore/AddressModal';
 import AddAddress from '@/components/Bookstore/AddAddress';
 
 import Image from 'next/image'
-import { useDispatch, useSelector, Provider  } from 'react-redux'
+import { useDispatch, useSelector, Provider } from 'react-redux'
 // import customerInfoReducer from 'redux/reducers/customerInfoReducer'
 import customerInfoAction from 'redux/actions/customerInfoAction'
 
 
 export default function addAddress() {
 
-  
+
   return (
     <>
+      <div className="flex flex-col h-full">
+        <header className="sticky bg-white top-0 z-99">
+          <MobileHeader Heading={'Address'}></MobileHeader>
+        </header>
 
-    <div className="flex flex-col h-full">
-      <header className="sticky bg-white top-0 z-99">  
-       <MobileHeader Heading={'Address'}></MobileHeader>
-      </header>
-     
-      <main className="overflow-auto h-full">
-        <AddAddress />
-      </main>
+        <main className="overflow-auto h-full">
+          <AddAddress />
+        </main>
 
-
-    </div>
-   
-
-     
-
-
-
-     
-      
+      </div>
     </>
   )
 }
