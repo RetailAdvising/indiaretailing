d271165204bab728ef6072d92af9e872532fb91b
Tridots <info@tridotstech.com>
2024-09-13 11:58:35 +0530

    13-09-24

--- components/Baners/Adsense.jsx
+++ components/Baners/Adsense.jsx
@@ -1,26 +1,34 @@
 import { useEffect } from 'react';
 
-const Adsense = ({ adSlot, adFormat = 'auto', adClient,adStyle }) => {
-  useEffect(() => {
-    try {
-      if (typeof window !== "undefined") {
-        (window.adsbygoogle = window.adsbygoogle || []).push({});
-      }
-    } catch (e) {
-      console.error("Adsbygoogle error", e);
-    }
-  }, []);
+const Adsense = ({ adSlot, adFormat = 'auto', adClient, adStyle }) => {
+    useEffect(() => {
+        try {
+            if (typeof window !== "undefined") {
+                (window.adsbygoogle = window.adsbygoogle || []).push({});
+            }
+        } catch (e) {
+            console.error("Adsbygoogle error", e);
+        }
+    }, []);
 
-//         data-ad-format={adFormat}
-  return (
-    <ins
-      className="adsbygoogle"
-      style={adStyle}
-    //   style={{ display: 'block' }}
-      data-ad-client={adClient}
-      data-ad-slot={adSlot}
-    ></ins>
-  );
+    //         data-ad-format={adFormat}
+    return (
+        <>
+            <script async
+                src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+                crossorigin="anonymous"></script>
+            <ins
+                className="adsbygoogle"
+                style={adStyle}
+                //   style={{ display: 'block' }}
+                data-ad-client={adClient}
+                data-ad-slot={adSlot}
+            ></ins>
+            <script>
+                (window.adsbygoogle = window.adsbygoogle || []).push({ });
+            </script>
+        </>
+    );
 };
 
 export default Adsense;
